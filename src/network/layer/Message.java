92aaae0311c656ada10d1e81a4bbbab834cd27cf
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-02-21 14:53:16 -0300

    Classe Message feita com sucesso

--- src/network/layer/Message.java
+++ src/network/layer/Message.java
@@ -15,5 +15,41 @@ public class Message implements Serializable{
 		this.params = params;
 	}
 	
+	public String getCommand() {
+		return command;
+	}
+
+	public String[] getParams(){
+		return params;
+	}
+	
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+	public static Message stringToMessage(String string){//transforma um string em um objeto mensagem
+		String[] split = string.split(":");
+		
+		String actor  = split[0];
+		String command  = split[1];
+		String[] params = new String[split.length-2];
+		
+		for(int i=2; i < split.length; i++){
+			 params [i-2]= split[i];
+		}
+			
+		return new Message(actor.charAt(0), command, params);
+		
+	}
+
+	public String toString(){ //transforma um objeto mensagem em um string
+		String msg = command;
+		
+		for (String p: params){
+			msg +=  ":" + p;
+		}
+		
+		return msg;
+	}
 	
 }
